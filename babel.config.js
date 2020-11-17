b594621de163a240db4ea9af3cb5f5a5933268f7
usama <marshchawki.mc@gmail.com>
2020-11-17 10:13:02 +0530

    checkFix: sentry ci fail

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,18 @@
-module.exports = function (api) {
-  const presets = ['module:metro-react-native-babel-preset'];
-  const plugins = [['module:react-native-dotenv']];
+// module.exports = function (api) {
+//   const presets = ['module:metro-react-native-babel-preset'];
+//   const plugins = [['module:react-native-dotenv']];
 
-  if (api.env('production')) {
-    plugins.push('module:metro-react-native-babel-preset');
-  }
+//   if (api.env('production')) {
+//     plugins.push('module:metro-react-native-babel-preset');
+//   }
 
-  return {
-    presets,
-    plugins,
-  };
+//   return {
+//     presets,
+//     plugins,
+//   };
+// };
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [['module:react-native-dotenv']],
 };
